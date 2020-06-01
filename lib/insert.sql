8b6aebff10acbc048f71f7a24cc27a5e2c3ba424
Chloe Steffens <caforonda103@yahoo.com>
2020-06-01 04:04:16 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'passive');
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Rowdy', 1, 'Female', 'Black and Brown', 'Wild');
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Tabitha', 2, 'Female', 'White and Black', 'Lazy');
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
+INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
 INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
