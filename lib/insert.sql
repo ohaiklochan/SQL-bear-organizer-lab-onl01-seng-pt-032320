50de39e8cc686f8e2b12371d8306557915745203
Chloe Steffens <caforonda103@yahoo.com>
2020-06-01 04:16:17 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'passive', 1);
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Rowdy', 1, 'Female', 'Black and Brown', 'Wild', 1);
-INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Tabitha', 2, 'Female', 'White and Black', 'Lazy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'passive', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 1, 'Female', 'Black and Brown', 'Wild', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 2, 'Female', 'White and Black', 'Lazy', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'Male', 'Black', 'Calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'Female', 'White', 'Calm', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'Brown', 'Grumpy', 1);
