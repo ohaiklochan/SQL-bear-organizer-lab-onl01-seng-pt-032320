bd64edda05dbe7483bed17388746992a5e8b4803
Chloe Steffens <caforonda103@yahoo.com>
2020-06-01 04:06:40 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'passive');
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Rowdy', 1, 'Female', 'Black and Brown', 'Wild');
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ('Tabitha', 2, 'Female', 'White and Black', 'Lazy');
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
-INSERT INTO bears (name, age, gender, color, temparament) VALUES ();
+INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Mr. Chocolate', 3, 'Male', 'Brown', 'passive', 1);
+INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Rowdy', 1, 'Female', 'Black and Brown', 'Wild', 1);
+INSERT INTO bears (name, age, gender, color, temparament, alive) VALUES ('Tabitha', 2, 'Female', 'White and Black', 'Lazy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 1, 'Male', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 5, 'Female', 'brown', 'calm', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 7, 'Male', 'brown', 'grumpy', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 9, 'F', 'brown', 'calm', 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 99, 'F', 'brown', 'deadly', 0)
